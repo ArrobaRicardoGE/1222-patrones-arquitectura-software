73baf49a45e9fea5d668e494e09937f4fed1514f
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-04-25 20:36:53 -0500

    [RutasTienda] Create QRCode adapter

---
+++ RutasTiendas/IronBarCodeAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronBarCode;
+
+namespace RutasTiendas
+{
+    class IronBarCodeAdapter : QRCodeGenerator
+    {
+        public override void GenerateQR(string data, string filename)
+        {
+            BarcodeWriter.CreateBarcode(data, BarcodeWriterEncoding.QRCode).SaveAsJpeg($"{filename}.jpg");
+        }
+
+        public override string ReadQR(string path)
+        {
+            BarcodeResult Result = BarcodeReader.QuicklyReadOneBarcode(path);
+            if (Result != null) return Result.Text;
+            throw new Exception("Invalid QR code"); 
+        }
+    }
+}
---
+++ RutasTiendas/QRCodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RutasTiendas
+{
+    abstract class QRCodeGenerator
+    {
+        public abstract void GenerateQR(string data, string route);
+        public abstract string ReadQR(string path); 
+    }
+}
